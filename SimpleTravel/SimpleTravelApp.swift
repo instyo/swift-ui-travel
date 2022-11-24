b564cdefc51b53516cd5b2e4e378c17af15529c8
Ikhwan <nsetyo19@gmail.com>
2022-11-24 22:21:18 +0700

    Initial Commit

---
+++ SimpleTravel/SimpleTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTravelApp.swift
+//  SimpleTravel
+//
+//  Created by Ikhwan on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
